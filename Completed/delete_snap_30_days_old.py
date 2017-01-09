5eabdabce004c705b9d1b5128a94ac65381ca95c
vazanth92 <vazanth92@gmail.com>
2017-01-09 22:02:48 +0530

    Update delete_snap_30_days_old.py

--- Completed/delete_snap_30_days_old.py
+++ Completed/delete_snap_30_days_old.py
@@ -14,31 +14,31 @@ deleted_snap_list = []
 error_list = []
 
 for snap in snapshot['Snapshots']:
-	id=snap['SnapshotId']
-	#print id
-	epoch=snap['StartTime'].strftime('%s')
-	result=int(epoch)
-	extra_epoch=result+one_month_epoch	
-	if extra_epoch<=current:
-		count = count + 1
-		snapshot_list.append(snap['SnapshotId'])
-		continue
-
+        id=snap['SnapshotId']
+        #print id
+        epoch=snap['StartTime'].strftime('%s')
+        result=int(epoch)
+        extra_epoch=result+one_month_epoch
+        if extra_epoch<=current:
+                snapshot_list.append(snap['SnapshotId'])
+                continue
+#print snapshot_list
 for snap in snapshot_list:
-	try:
-		client.delete_snapshot(SnapshotId=snap)
-		deleted_snap_list.append(snap)
-	except Exception,e:
-		error_list.append(str(e))
-	continue
-	
-#count number of deleted snapshots	
+        try:
+#                client.describe_snapshots(SnapshotIds=[snap])
+                client.delete_snapshot(SnapshotId=snap)
+                deleted_snap_list.append(snap)
+        except Exception,e:
+                error_list.append(str(e))
+        continue
+
+#count number of deleted snapshots
 count_a = len(deleted_snap_list)
 #count number of error occured
 count_error_list = len(error_list)
 
 mystring_1 = str(deleted_snap_list)
-mystring_1 = mystring_1.replace(",", "\n")	
+mystring_1 = mystring_1.replace(",", "\n")
 
 if count_error_list != 0:
         c = "\n An error occured while trying to delete the snapshot "+str(error_list)+" \n"
@@ -46,6 +46,7 @@ if count_error_list != 0:
 a = "the count of deleted snapshots are: "+str(count_a)+"\n"
 b = "The deleted snapshots were :\n"+mystring_1+"\n"
 output = a+b+c
+#print output
 
 #SNS to notify the result of Delete_snapshot call
 sns_client = boto3.client('sns',region)
